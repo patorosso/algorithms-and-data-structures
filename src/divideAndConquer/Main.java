52e7b148cc9ffc59cce2542395912a470e3bf526
patricio-rosso <patriciosimonrosso@gmail.com>
2024-06-13 20:53:35 -0300

    add power function (todo)

--- src/divideAndConquer/Main.java
+++ src/divideAndConquer/Main.java
@@ -4,16 +4,26 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int elemToFind = 200;
-		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 }; // pre-sorted array of positive ints
-		BinarySearch binarySearcher = new BinarySearch();
+		// Binary Search
 
-		int result = binarySearcher.doIt(array, elemToFind);
+//		int elemToFind = 200;
+//		int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 }; // pre-sorted array of positive ints
+//		BinarySearch binarySearcher = new BinarySearch();
+//
+//		int result = binarySearcher.doIt(array, elemToFind);
+//
+//		if (result != -1) {
+//			System.out.println("Gotcha! " + elemToFind + " was found!" );
+//		} else
+//			System.out.println(elemToFind + " was not found!");
 
-		if (result != -1) {
-			System.out.println("Gotcha! " + elemToFind + " was found!" );
-		} else
-			System.out.println(elemToFind + " was not found!");
+		// ------------------
+
+		// Power
+
+		double number = 4;
+		int exponent = 2;
+		System.out.println(number + " to the power of " + exponent + " equals: " + Power.doIt(4, 2));
 
 	}
 }
---
+++ src/divideAndConquer/Power.java
@@ -0,0 +1,12 @@
+package divideAndConquer;
+
+public class Power {
+	
+	public static double doIt(double number, int exponent) {
+		return 1;
+	}
+	
+	// hidden power function
+	//private double power(double number, double exponent)
+	
+}
