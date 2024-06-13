8198c01112ae99519a4403d2d66c4173bcaa3d82
patricio-rosso <patriciosimonrosso@gmail.com>
2024-06-13 14:01:22 -0300

    create Heap class

--- src/priorityQueue/Heap.java
+++ src/priorityQueue/Heap.java
@@ -1,9 +1,33 @@
 package priorityQueue;
 
+// an implementation of a min Heap using an array
+
 public class Heap {
 
-	public Heap() {
-		
-	}
+	private int size;
+	private int[] heapArray;
 
+	public Heap(int capacity) throws Exception {
+		if (capacity <= 0)
+			throw new Exception("Heap size must be > 0.");
+
+		size = 0;
+		heapArray = new int[capacity];
+	}
+	
+	
+	// -- public methods --
+	
+	// get
+	
+	// insert
+	
+	// remove
+
+	// -- private ones --
+	
+	
+	
+	
+	
 }
