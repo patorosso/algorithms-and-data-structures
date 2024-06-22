1c2363c7ff3fc35b5e32a74140519556d03a0a88
patricio-rosso <patriciosimonrosso@gmail.com>
2024-06-21 23:51:29 -0300

    add house thief problem

--- code/src/dynamicProgramming/DynamicProgramming.java
+++ code/src/dynamicProgramming/DynamicProgramming.java
@@ -6,14 +6,13 @@ public class DynamicProgramming {
 
 	public static int fibonacci(int n) {
 
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-		int result = fibonacci(map, n);
+		// int result = fibonacciTopDown(new HashMap<Integer, Integer>(), n);
+		int result = fibonacciBottomDown(new int[n + 1], n);
 
 		return result;
 	}
 
-	private static int fibonacci(HashMap<Integer, Integer> map, int n) {
+	private static int fibonacciTopDown(HashMap<Integer, Integer> map, int n) {
 
 		if (map.containsKey(n))
 			return map.get(n);
@@ -23,8 +22,51 @@ public class DynamicProgramming {
 			return n;
 		}
 
-		map.put(n, fibonacci(map, n - 1) + fibonacci(map, n - 2));
+		map.put(n, fibonacciTopDown(map, n - 1) + fibonacciTopDown(map, n - 2));
 		return map.get(n);
+	}
+
+	private static int fibonacciBottomDown(int[] array, int n) {
+
+		if (n < 2)
+			return n;
+
+		// array[0] = 0; not needed
+		array[1] = 1;
+
+		// we could do this, but as fibonacci only needs the 2 previous values of n,
+		// there's no need to keep in the table all previous values.
+		// we could just use aux variables to store those values.
+		for (int i = 2; i <= n; i++)
+			array[i] = array[i - 1] + array[i - 2];
+
+		return array[n];
+	}
+
+	// bottom down approach
+	public static int houseThief(int[] houses) {
+
+		int length = houses.length;
+
+		if (length == 0)
+			return 0;
+
+		if (length == 1)
+			return houses[0];
+
+		if (length == 2)
+			return Integer.max(houses[0], houses[1]);
+
+		// initialize loot array
+		int[] loot = new int[length];
+		loot[0] = houses[0];
+		loot[1] = Math.max(houses[0], houses[1]);
+
+		// fill remaining positions
+		for (int i = 2; i < length; i++)
+			loot[i] = Math.max(houses[i] + loot[i - 2], loot[i - 1]);
+
+		return loot[length - 1];
 	}
 
 }
--- code/src/dynamicProgramming/Main.java
+++ code/src/dynamicProgramming/Main.java
@@ -4,10 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int number = 3;
-		int result = DynamicProgramming.fibonacci(number);
-
-		System.out.println("Fibonacci number of " + number + " is: " + result);
+//		int number = 9;
+//		int result = DynamicProgramming.fibonacci(number);
+//
+//		System.out.println("Fibonacci number of " + number + " is: " + result);
+		
+		int[] houses = {1000,9,3,1000,8,1,7};
+		int result = DynamicProgramming.houseThief(houses);
+		
+		System.out.println("House thief could steal: " + result);
 	}
 
 }
