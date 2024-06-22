61d9cab277528120ec9b1e3c7440885a558fde48
patricio-rosso <patriciosimonrosso@gmail.com>
2024-06-21 21:11:50 -0300

    add dynamic programming code with fibonacci example

---
+++ code/src/dynamicProgramming/DynamicProgramming.java
@@ -0,0 +1,29 @@
+package dynamicProgramming;
+
+import java.util.*;
+
+public class DynamicProgramming {
+
+	public static int fibonacci(int n) {
+
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		int result = fibonacci(map, n);
+
+		return result;
+	}
+
+	private static int fibonacci(HashMap<Integer, Integer> map, int n) {
+
+		if (map.containsKey(n))
+			return map.get(n);
+
+		if (n < 2) {
+			map.put(n, n);
+			return n;
+		}
+
+		return fibonacci(map, n - 1) + fibonacci(map, n - 2);
+	}
+
+}
---
+++ code/src/dynamicProgramming/Main.java
@@ -0,0 +1,13 @@
+package dynamicProgramming;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int number = 3;
+		int result = DynamicProgramming.fibonacci(number);
+
+		System.out.println("Fibonacci number of " + number + " is: " + result);
+	}
+
+}
