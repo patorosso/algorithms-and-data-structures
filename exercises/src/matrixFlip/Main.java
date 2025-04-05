0b2be97bfac35cf71e1fb4197514680512e23f53
patricio-rosso <patriciosimonrosso@gmail.com>
2025-04-05 16:05:02 -0300

    fix the flip first approach to work with other NxN matrixes

--- exercises/src/matrixFlip/Main.java
+++ exercises/src/matrixFlip/Main.java
@@ -4,16 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		
-		//int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+//		int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 		int[][] matrix = {
 			    {  5,  1,  9, 11 },
 			    {  2,  4,  8, 10 },
 			    { 13,  3,  6,  7 },
 			    { 15, 14, 12, 16 }
 			};
-		
-		
+
 		flip90firstApproach(matrix);
 		printMatrix(matrix);
 
@@ -21,19 +19,21 @@ public class Main {
 
 	private static void flip90firstApproach(int[][] matrix) {
 
-		int aux1, currentN;
+		int aux1;
+		int n = matrix.length;
+		
+		// Comments are for my own, used at debugging the 3x3 example, but works
+		// for other N's as well.
 
 		for (int j = 0; j < matrix.length / 2; j++) {
 
-			currentN = matrix.length - (j * 2); // Para legibilidad
+			for (int i = 0; i < matrix.length - 1 - (j*2); i++) {
 
-			for (int i = 0; i < matrix.length - 1; i++) {
-
-				aux1 = matrix[j][j + i]; // Guardo el 1
-				matrix[j][j + i] = matrix[currentN - i - 1][j]; // Pongo el 7 en el 1
-				matrix[currentN - i - 1][j] = matrix[currentN - j - 1][currentN - 1 - i]; // 9 en el 7
-				matrix[currentN - j - 1][currentN - 1 - i] = matrix[i][currentN - j - 1]; // 3 en el 9
-				matrix[i][currentN - j - 1] = aux1;
+				aux1 = matrix[j][j + i]; // Save the 1
+				matrix[j][j + i] = matrix[n - i - 1 - j][j]; // Place the 7 on the 1
+				matrix[n - i - 1 - j][j] = matrix[n - j - 1][n - 1 - i - j]; // 9 on the 7
+				matrix[n - j - 1][n - 1 - i - j] = matrix[i + j][n - j - 1]; // 3 on the 9
+				matrix[i + j][n - j - 1] = aux1;
 			}
 		}
 	}
@@ -42,7 +42,8 @@ public class Main {
 
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++)
-				System.out.print(matrix[i][j] + " ");
+			System.out.print((matrix[i][j] > 9 ? matrix[i][j] : "0" + matrix[i][j]) + " ");
+				
 			System.out.println();
 
 		}
